f033dcc58aa08efd923b8789ea661d0b4c9db600
lizhiping <brigg2008@126.com>
2025-01-22 14:19:58 +0800

    update numpy

--- new/binaryClassifier_singleInput.py
+++ new/binaryClassifier_singleInput.py
@@ -1,9 +1,10 @@
-import tensorflow as tf 
+
 from keras.datasets import imdb
 import matplotlib.pyplot as plt
 from keras import models
 from keras import layers
-
+import tensorflow as tf
+import numpy as np
 # load data
 (train_data,train_labels),(test_data,test_labels) = imdb.load_data(num_words=10000)
 # network
